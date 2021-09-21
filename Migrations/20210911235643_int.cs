ebd87d0735c5f3766f4ab16f58e671ce79427014
Admin <Admin@LAPTOP-798FHRV5>
2021-09-21 21:06:01 +1200

    Add project files.

---
+++ Migrations/20210911235643_int.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Web_API_Car.Migrations
+{
+    public partial class @int : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Model",
+                table: "Car",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Make",
+                table: "Car",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Model",
+                table: "Car",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Make",
+                table: "Car",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
---
+++ Model/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web_API_Car.Model
+{
+    // models represents the information of car
+    public class Car
+    {
+        public string Id { get; set; }
+        // tells year of car in which manufactured
+        [Required ]
+        public int Year { get; set; }
+        // tells the make of car
+        [Required]
+        public string Make { get; set; }
+        // tells the of model of car
+        [Required]
+        public string Model { get; set; }
+        // tells the price of car
+        [Required]
+        public decimal Price { get; set; }
+
+
+
+    }
+}
